841d80e295704381eb34a6177179e186d0f4f975
rainwashed <dementia.aswad@gmail.com>
2025-03-29 21:57:48 -0400

    new icon

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# StarshotOCR
+<p align="center">
+    <img src="./src/static/icons/starshot/Starshot.svg" width="100">
+    <h1 align="center">StarshotOCR</h1>
+</p>
+
 An open-source, powerful, and intelligent OCR application powered by Tesseract from Google.
--- src/routes/Startup.vue
+++ src/routes/Startup.vue
@@ -5,6 +5,8 @@ import {ref} from "vue";
 import { useRouter } from "vue-router";
 import { Button } from "@/components/ui/button";
 
+import StarshotIconSVG from "../static/icons/starshot/Starshot.svg"
+
 const currentPage = ref(0);
 const incrementPage = () => currentPage.value += 1;
 const decrementPage = () => currentPage.value -= 1;
@@ -19,6 +21,7 @@ const skipNonsense = () => {
     <Transition name="slide-fade">
         <div class="w-screen h-screen pt-9"> 
             <div class="flex flex-col items-center justify-center h-full" v-if="currentPage === 0">
+              <img :src="StarshotIconSVG" class="w-auto h-48 aspect-square" draggable="false" />
                 <h1 class="text-3xl font-bold">Welcome to StarshotOCR</h1>
                 <p class="mb-2">An open-source, powerful, and intelligent OCR application powered by Tesseract.</p>
                 <span class="inline-flex mb-8 space-x-1 text-2xl">
